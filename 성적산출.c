9bbb8543233b0b46f6d96b4ffb5d76c7cca4ccbc
김재현 <165982669+cass2510@users.noreply.github.com>
2024-04-07 04:24:41 +0900

    Update 성적산출.c

--- 성적산출.c
+++ 성적산출.c
@@ -2,15 +2,15 @@
 int main(void){
 	FILE* fp_read = fopen("sj.txt", "r");
 	FILE* fp_write = fopen("sj1.txt", "w");
-	//필요파일정의및열어놓기 
+	//필요 파일 정의 및 열어놓기 
 	
 	char name1[256], name2[256];
 	int score[3], sum = 0, i;
-	// sj.txt에있는이름과정수정의하기 
+	// sj.txt에 있는 이름과 정수 정의하기 
 	 
 	fscanf(fp_read, "%s %s", name1, name2);
 	fprintf(fp_write, "%s %s\n", name1, name2);
-	// sj.txt에있는내용을name1, name2에저장및sj1.txt에작성하기 
+	// sj.txt에 있는 내용을 name1, name2에 저장 및 sj1.txt에 작성하기 
 	
 	for(i = 0; i < 3; i++) {
 		fscanf(fp_read, "%d", &score[i]);
@@ -18,11 +18,11 @@ int main(void){
 		fprintf(fp_write, "%d\n", score[i]);
 	}
 	fprintf(fp_write, "\n%.2f", sum / 3.0);
-	// sj.txt파일에저장된숫자를입력받고 평균을계산하여sj1.txt에저장 
+	// sj.txt파일에 저장된 숫자를 입력받고 평균을 계산하여 sj1.txt에 저장 
 	
 	fclose(fp_read);
 	fclose(fp_write);
-	// 실행중인파일닫기 
+	// 실행중인 파일 닫기 
 
 return 0;
 }
